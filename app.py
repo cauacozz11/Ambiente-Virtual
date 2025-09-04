2b53a0580066af98be420cea99aba5d4a3c5f601
cauacozz11 <cauacozz@gmail.com>
2025-09-04 16:19:09 -0300

    comit final

--- app.py
+++ app.py
@@ -1,5 +1,34 @@
 import requests
-
+import json
 
 url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
 
+response = requests.get(url)
+
+
+if response.status_code == 200:
+    dados_json = response.json()
+    dados_resutarante = {}
+    for item in dados_json:
+        nome_do_restaurante = item['Company']
+        if nome_do_restaurante not in dados_resutarante:
+            dados_resutarante[nome_do_restaurante] = []
+            
+        dados_resutarante[nome_do_restaurante].append({
+            "item": item['Item'],
+            "price": item['price'],
+            "description": item['description']
+        })         
+    
+
+else:
+     print(f'O erro foi {response.status_code}')    
+     
+
+
+print(dados_resutarante['Pizza Hut'])
+
+for nome_do_restaurante, dados in dados_resutarante.items():
+    nome_do_arquivo = f'{nome_do_restaurante}.json'
+    with open(nome_do_arquivo, 'w') as arquivo_restaurante:
+        json.dump(dados, arquivo_restaurante, indent=4)
